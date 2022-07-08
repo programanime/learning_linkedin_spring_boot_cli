0672e8ad44eae86cc0a784779826cbecb203770e
Daniel Molina Yepes <programanime@gmail.com>
2022-07-08 06:51:13 -0500

    Initial commit

---
+++ src/main/java/co/iacode/cli/Room.java
@@ -0,0 +1,16 @@
+package co.iacode.cli;
+import lombok.Data;
+
+@Data
+public class Room{
+    private long id;
+    private String number;
+    private String name;
+    private String info;
+    public Room(long id, String number, String name, String info){
+        this.id=id;
+        this.number=number;
+        this.name=name;
+        this.info=info;
+    }
+}
---
+++ src/test/java/co/iacode/cli/CliApplicationTests.java
@@ -0,0 +1,13 @@
+package co.iacode.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
